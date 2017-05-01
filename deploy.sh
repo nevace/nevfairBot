f88c5735b98511cfaeb32588369ca53d5bd89991
nevace <liam.neville89@gmail.com>
2017-05-01 13:42:24 +0100

    test ssh 11

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ mkdir -p /home/nevace/.ssh
 ssh-keyscan -t rsa 139.59.178.69 > ~/.ssh/known_hosts
 echo $PRIVATE_SSH_KEY >> /home/nevace/.ssh/id_rsa
 chmod -R 700 /home/nevace/.ssh
-ssh -f nevace@139.59.178.69 touch hi6.txt
+ssh nevace@139.59.178.69 touch hi6.txt && exit
