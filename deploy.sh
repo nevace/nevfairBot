c78b349a8c28a31fe8bf7aa3bb3c8ac2fddbe521
nevace <liam.neville89@gmail.com>
2017-05-01 13:04:17 +0100

    test ssh 5

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "Getting latest code"
-echo ""
-cd "nevfairBot"
-git pull
+#echo "Getting latest code"
+#echo ""
+#cd "nevfairBot"
+#git pull
+
+#mkdir -p /home/nevace/.ssh && \ ssh-keyscan -t rsa 139.59.178.69 > ~/.ssh/known_hosts && \ echo $PRIVATE_SSH_KEY >> /home/nevace/.ssh/id_rsa && \ chmod -R 700 /home/nevace/.ssh && \ cat /home/nevace/.ssh/id_rsa && \ ssh nevace@139.59.178.69 touch hi4.txt
+
+mkdir -p /home/nevace/.ssh
+ssh-keyscan -t rsa 139.59.178.69 > ~/.ssh/known_hosts
+echo $PRIVATE_SSH_KEY >> /home/nevace/.ssh/id_rsa
+chmod -R 700 /home/nevace/.ssh
+cat /home/nevace/.ssh/id_rsa
+ssh nevace@139.59.178.69 touch hi4.txt
